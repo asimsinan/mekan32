fd4de24b284fd98a122f35321e48a65784f8c477
asimsinanyuksel <a.sinanyuksel@gmail.com>
2020-11-27 17:28:53 +0300

    version 3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,10 +1,76 @@
 
 const anaSayfa=function(req, res, next) {
-  res.render('mekanlar-liste', { title: 'Anasayfa' });
+  res.render('mekanlar-liste', 
+  	{ 'baslik': 'Anasayfa',
+  	'sayfaBaslik':{
+  		'siteAd':'Mekan32',
+  		'aciklama':'Isparta civarındaki mekanları keşfedin'
+  	},
+    'mekanlar':[
+    {
+      'ad':'Starbucks',
+      'adres':'Centrum Garden AVM',
+      'puan':3,
+      'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
+      'mesafe':'10km'
+    },
+      {
+      'ad':'Gloria Jeans',
+      'adres':'SDÜ Doğu Kampüsü',
+      'puan':2,
+      'imkanlar':['Kahve','Çay','Pasta'],
+      'mesafe':'1km'
+    },
+    ]
+  }
+
+  	);
 }
 
 const mekanBilgisi=function(req, res, next) {
-  res.render('mekan-detay', { title: 'Mekan Bilgisi'});
+  res.render('mekan-detay', 
+    { 
+    'baslik': 'Mekan Bilgisi',
+    'sayfaBaslik':'Starbucks',
+    'mekanBilgisi':{
+      'ad':'Starbucks',
+      'adres':'Centrum Garden AVM',
+      'puan':3,
+      'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
+      'koordinatlar':{
+          'enlem':'37.781885',
+          'boylam':'30.566034'
+      },
+      'saatler':[
+        {
+          'gunler':'Pazartesi-Cuma',
+          'acilis':'7:00',
+          'kapanis':'23:00',
+          'kapali':false
+        },
+        {
+          'gunler':'Cumartesi',
+          'acilis':'9:00',
+          'kapanis':'22:00',
+          'kapali':false
+        },
+        {
+          'gunler':'Pazar', 
+          'kapali':true
+        }
+      ],
+      'yorumlar':[
+        {
+          'yorumYapan':'Asım Sinan Yüksel',
+          'puan':3,
+          'tarih':'27.11.2020',
+          'yorumMetni':'Kahveleri güzel.'
+        }
+      ]
+
+    }
+  }
+    );
 }
 
 const yorumEkle=function(req, res, next) {
